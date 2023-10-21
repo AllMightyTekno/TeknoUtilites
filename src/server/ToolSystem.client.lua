8029eeff6cd9080c41eb671f60821ff1e0cf4fa2
Wboy0010 <143533874+AllMightyTekno@users.noreply.github.com>
2023-10-21 22:31:14 +0100

    Final Rope Tool System

---
+++ src/server/ToolSystem.client.lua
@@ -0,0 +1,15 @@
+script.Parent.Activated:Connect(function()
+	local mouse = game.Players.LocalPlayer:GetMouse()
+	script.Parent.RemoteEvent:FireServer(mouse.Target,mouse.Hit)
+   
+    mouse.Move:Connect(function()
+       local Target = mouse.Target
+    end)
+
+        if mouse.Target and mouse.Target.Parent:FindFirstChild("Humanoid") then
+            local CharacterModel = mouse.Target.Parent --Every character with a Humanoid is set to the CharacterModel Variable
+            mouse.Target.Parent = CharacterModel
+            warn('ROPE ATTACHED TO '..mouse.Target.Parent.Name)
+end
+
+end)
